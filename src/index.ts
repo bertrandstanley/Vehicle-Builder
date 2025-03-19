1646647f84a9d5a1573a36c62850cd5de0393bf7
bertrandstanley <bertrandstanley@gmail.com>
2025-03-18 20:06:45 -0400

    Added an express server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import classes
+import express from 'express';
 import Truck from "./classes/Truck.js";
 import Car from "./classes/Car.js";
 import Motorbike from "./classes/Motorbike.js";
@@ -6,34 +6,75 @@ import Wheel from "./classes/Wheel.js";
 import Cli from "./classes/Cli.js";
 import Vehicle from "./classes/Vehicle.js";
 
-const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+// Create an instance of the express app
+const app = express();
+const port = process.env.PORT || 3000;  // Set the port for the server
 
-// will use default wheels
+// Create vehicle instances
+const truck1 = new Truck(
+  Cli.generateVin(),
+  "red",
+  "Ford",
+  "F-150",
+  2021,
+  5000,
+  120,
+  [],
+  10000
+);
+
 const car1 = new Car(
   Cli.generateVin(),
-  'blue',
-  'Toyota',
-  'Camry',
+  "blue",
+  "Toyota",
+  "Camry",
   2021,
   3000,
   130,
   []
 );
 
-const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
-const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+const motorbike1Wheels = [
+  new Wheel(17, "Michelin"),
+  new Wheel(17, "Michelin")
+];
+const motorbike1 = new Motorbike(
+  Cli.generateVin(),
+  "black",
+  "Harley Davidson",
+  "Sportster",
+  2021,
+  500,
+  125,
+  motorbike1Wheels
+);
 
-// create an array of vehicles
-const vehicles: Vehicle[] = []
+// Create an array to store all vehicles
+const vehicles: Vehicle[] = [truck1, car1, motorbike1];
 
-// push vehicles to array
-vehicles.push(truck1);
-vehicles.push(car1);
-vehicles.push(motorbike1);
-
-
-// create a new instance of the Cli class
+// Create an instance of the Cli class (CLI functionality is not required for the server, but it can still be created)
 const cli = new Cli(vehicles);
 
-// start the cli
-cli.startCli();
+// Define API Route: Get all vehicles
+app.get('/api/vehicles', (req, res) => {
+  res.json(vehicles);  // Return the array of vehicles as a JSON response
+});
+
+// Define API Route: Get a specific vehicle by VIN
+app.get('/api/vehicle/:vin', (req, res) => {
+  const vin = req.params.vin;
+  const vehicle = vehicles.find(v => v.vin === vin);
+  
+  if (vehicle) {
+    res.json(vehicle);  // Return the found vehicle as a JSON response
+  } else {
+    res.status(404).send('Vehicle not found');  // If vehicle with the given VIN is not found
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+  // Optionally start the CLI if needed (though it's not typically used in a web app)
+  cli.startCli();
+});
